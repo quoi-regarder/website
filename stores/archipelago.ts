29e43646c4fb0b66e2f21953abeeb5daaf4e9cb4
lapizzamolo <lapizzamolo@MSI.lan>
2025-02-20 11:28:05 +0100

    Feat(popin/archipelago.vue): Add popup for archilan events (only weeks)

---
+++ stores/archipelago.ts
@@ -0,0 +1,19 @@
+export const useArchipelagoStore = defineStore('archipelago', {
+  state: () => ({
+    showPopin: true
+  }),
+  getters: {
+    getShowPopin: (state) => state.showPopin
+  },
+  actions: {
+    setShowPopin (value: boolean) {
+      this.showPopin = value
+    }
+  },
+  persist: {
+    storage: piniaPluginPersistedstate.cookies({
+      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      secure: true
+    })
+  }
+})
