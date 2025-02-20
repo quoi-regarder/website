5878815086be89db8c1c46e6923a167e5344ba25
William Fontaine <william.fontaine0308@gmail.com>
2025-02-20 10:53:32 +0100

    #188: Filter overflow fix (#189)

--- layouts/filter.vue
+++ layouts/filter.vue
@@ -2,7 +2,7 @@
   <div
     class="w-full p-2 gap-4 flex flex-col xl:flex-row rounded-2xl bg-[var(--ui-bg-elevated)] dark:bg-[var(--ui-bg-muted)]"
   >
-    <aside class="w-full flex justify-between flex-row gap-4 xl:flex-col xl:w-1/3 2xl:w-1/4">
+    <aside class="w-full flex justify-between flex-row gap-4 xl:flex-col xl:w-[35%] 2xl:w-1/4">
       <div
         class="flex flex-col xl:items-justify xl:w-full"
         :class="hasButtons ? 'w-1/2' : 'w-full'"
@@ -43,7 +43,7 @@
 
     <USeparator orientation="vertical" class="hidden xl:block" />
 
-    <div class="w-full xl:w-2/3 2xl:w-3/4 flex flex-row items-start">
+    <div class="w-full xl:w-[65%] 2xl:w-3/4 flex flex-row items-start">
       <slot name="content" />
     </div>
   </div>
